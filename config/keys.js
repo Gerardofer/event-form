1c09c976b1dbc704c0404baa758c863afa0cbc63
brylin142 <33795878+brylin142@users.noreply.github.com>
2018-11-07 19:44:45 -0800

    Set up database

---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+  mongoURI: 'mongodb://eventform:password1@ds155203.mlab.com:55203/form-db'
+};
--- index.js
+++ index.js
@@ -1,11 +1,32 @@
 const express = require("express");
+const mongoose = require('mongoose');
+const keys = require('./config/keys');
+// const Users = require('./models/User');
 const app = express();
 
-const PORT = process.env.PORT || 8080;
+// mongoose.connect(keys.mongoURI);
+mongoose.connect('mongodb://eventform:password1@ds155203.mlab.com:55203/form-db');
 
-app.get("/", (req, res) => {
-  res.send({ name: "Gerardo Fernandez" });
+const userSchema = new mongoose.Schema({
+  googleId: String
 });
+
+const Users = mongoose.model('users', userSchema);
+
+Users.create({
+  googleId: "sdfglkjhsdfgkjh123"
+}, (err, createdID) => {
+  if (err) {
+    return err;
+  } else {
+    console.log(createdID);
+  }
+});
+
+app.get("/user", (req, res) => {
+  res.send({name: 'Gerardo Fernandez'})
+});
+const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, () => {
   console.log("Port listening on", PORT);
